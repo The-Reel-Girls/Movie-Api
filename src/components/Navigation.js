ad51ef9f01257d8fddd2e448c80ef81d491ac0cd
Ahn Shields <ahnshields@gmail.com>
2023-05-04 11:03:29 -0700

    Add contents

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import squirtle from '../../public/squirtle.jpg';
+
+function Navigation() {
+  return (
+    <Navbar sticky='top' bg='dark' variant='dark' className='mb-4'>
+      <Container>
+        <Navbar.Brand>
+          <Image src={squirtle} width='30' className='me-2' />
+          Pokeverse
+        </Navbar.Brand>
+
+        <Nav className='me-auto'>
+          <Nav.Link as="div">
+            <NavLink to='/'>All Pokemon</NavLink>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export { Navigation };
---
+++ src/components/PokemonCard.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+
+function PokemonCard({ name }) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [name]);
+
+  return (
+    <Card style={{ width: '18rem' }} className='mx-auto'>
+      <Card.Img
+        width='286'
+        height='286'
+        bg='dark'
+        variant='top'
+        src={pokemon?.sprites.front_default}
+      />
+      <Card.Body>
+        <Card.Title>
+          <Link to={`/${name}`}>{name}</Link>
+        </Card.Title>
+        <Card.Text as='div'>
+          Abilities:
+          <ul>
+            {pokemon?.abilities.map((ability) => (
+              <li key={ability.ability.name}>
+                <span key={ability.ability.name}>{ability.ability.name}</span>
+              </li>
+            ))}
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export { PokemonCard };
