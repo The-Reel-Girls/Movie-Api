80ac4cd22c633c2985a800ea0bed22838c2e8ad2
Azeb Haile <azebhaile90@gmail.com>
2023-05-15 13:27:24 -0400

    api connected

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
-
+import axios from "axios";
 function MovieCard({ name }) {
   const [movie, setmovie] = useState(null);
-
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=35e1b4f6&y=2023`)
-      .then((res) => res.json())
-      .then((data) => {
-        setmovie(data);
+    const api_key = "35e1b4f6";
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ed6644c047164beb3f3d1168199545df&query=${name}`
+      )
+      .then(function (response) {
+        console.log(response.data.results);
+        if (response.data.results.length > 0) {
+          setmovie(response.data.results.at(-1));
+        }
       })
-      .catch((error) => {
-        console.error(error);
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
       });
+    // fetch(`http://www.omdbapi.com/?apikey=${api_key}&t=${name}`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //    console.log(data);
+    //     //setmovie(data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   }, [name]);
-
   return (
     <Card style={{ width: "18rem" }} className="mx-auto">
       <Card.Img
@@ -23,7 +39,7 @@ function MovieCard({ name }) {
         height="286"
         bg="dark"
         variant="top"
-        src={movie?.sprites.front_default}
+        src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
       />
       <Card.Body>
         <Card.Title>
@@ -32,16 +48,15 @@ function MovieCard({ name }) {
         <Card.Text as="div">
           Abilities:
           <ul>
-            {movie?.abilities.map((ability) => (
+            {/* {movie?.abilities.map((ability) => (
               <li key={ability.ability.name}>
                 <span key={ability.ability.name}>{ability.ability.name}</span>
               </li>
-            ))}
+            ))} */}
           </ul>
         </Card.Text>
       </Card.Body>
     </Card>
   );
 }
-
 export { MovieCard };
