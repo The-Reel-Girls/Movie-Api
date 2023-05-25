88347c6c1da58233dd9ba1d1bc5c7e3b0935bdc9
Azeb Haile <azebhaile90@gmail.com>
2023-05-25 06:34:44 -0400

    added search and toggle

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import squirtle from '../../public/squirtle.jpg';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import squirtle from "../../public/squirtle.jpg";
 
-function Navigation() {
+function Navigation({ changeTheme, theme }) {
   return (
-    <Navbar sticky='top' bg='dark' variant='dark' className='mb-4'>
+    <Navbar sticky="top" bg={theme} variant={theme} className="mb-4">
       <Container>
         <Navbar.Brand>
-          <Image src={squirtle} width='30' className='me-2' />
+          <Image src={squirtle} width="30" className="me-2" />
           Movies
         </Navbar.Brand>
 
-        <Nav className='me-auto'>
+        <Nav className="me-auto">
           <Nav.Link as="div">
-            <NavLink to='/'>All Movies</NavLink>
+            <NavLink to="/">All Movies</NavLink>
           </Nav.Link>
         </Nav>
+
+        <div>
+          <input type="checkbox" onChange={changeTheme} id="theme-toggle" />
+          <label htmlFor="theme-toggle"> Toggle Theme</label>
+        </div>
       </Container>
     </Navbar>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,23 +5,35 @@ import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 import { MovieCard } from "../components";
+import axios from "axios";
 
-
-function Home({ movieList }) {
+function Home({ theme, movieList }) {
   console.log(movieList);
   const [filteredMovie, setFilteredMovie] = useState([]);
   const [search, setSearch] = useState("");
   console.log(movieList);
   useEffect(() => {
-    setFilteredMovie(
-      movieList.filter((movie) =>
-        movie.original_title.toLowerCase().includes(search.toLowerCase())
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ed6644c047164beb3f3d1168199545df&query=${search}`
       )
-    );
+      .then(function (response) {
+        console.log(response.data);
+        if (response.data && response.data.results.length > 0) {
+          const moviesWithImages = response.data.results.filter(
+            (_m) => _m.backdrop_path !== null
+          );
+
+          setFilteredMovie(moviesWithImages);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, [search, movieList]);
 
   return (
-    <Container>
+    <Container bg={theme} variant={theme}>
       <Row className="mb-4">
         <Col sm="8" md="6" className="mx-auto">
           <InputGroup>
@@ -37,11 +49,17 @@ function Home({ movieList }) {
       </Row>
 
       <Row className="g-4">
-        {filteredMovie.map((movie) => (
-          <Col key={movie.original_title}>
-            <MovieCard moviedetails={movie} />
-          </Col>
-        ))}
+        {search.length > 0
+          ? filteredMovie.map((movie) => (
+              <Col key={movie.original_title}>
+                <MovieCard moviedetails={movie} />
+              </Col>
+            ))
+          : movieList.map((movie) => (
+              <Col key={movie.original_title}>
+                <MovieCard moviedetails={movie} />
+              </Col>
+            ))}
       </Row>
     </Container>
   );
